077a8cb05bffc585c7ef567dfeb0f5ebe4b0b6bf
m0tholith <48518572+m0tholith@users.noreply.github.com>
2024-10-16 19:36:33 +0300

    Got shit to work

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from dataclasses import dataclass
+from enum import Enum
 
 import usb.core
 import usb.util
-
-PACKET_SIZE = 8
 
 
 class deviceInfo:
@@ -14,78 +13,44 @@ class deviceInfo:
     wIndex = 0x0307
 
 
-@dataclass
-class PacketPack:
-    size: int
-    packets: list[list[int]]
+class key(Enum):
+    LMB = 0x01
+    MMB = 0x02
+    RMB = 0x03
+    FORWARD = 0x04
+    BACKWARD = 0x05
+    PLUS = 0x08
+    MINUS = 0x06
 
 
-def get_packets_to_send() -> list[PacketPack]:
-    packetsFile = open(r"./packets", "r")
-    packetsStr: list[str] = packetsFile.readlines()
+class keybind(Enum):
+    LEFTCLICK = 0x01
+    MIDDLECLICK = 0x02
+    RIGHTCLICK = 0x03
+    FORWARD = 0x04
+    BACKWARD = 0x05
+    DPI_LOOP = 0x06
+    SHOW_DESKTOP = 0x07
+    DOUBLE_LEFTCLICK = 0x08
+    FIRE = 0x09
+    OFF = 0x0A
+    DPI_PLUS = 0x0B
+    DPI_MINUS = 0x0C
 
-    packetPackCount: int = 1 + packetsStr.count("\n")
 
-    packets: list[list[int]] = []
-    for i in range(len(packetsStr)):
-        packetsStr[i] = (packetsStr[i])[0:-1]
-        if len(packetsStr[i]) <= 1:
-            continue
-        hexarr: list[int] = [int(j, 16) for j in packetsStr[i].split()]
-        packets.append(hexarr)
-
-    packetPackSize = len(packets) // packetPackCount
-
-    packetsFile.close()
-
-    return [
-        PacketPack(
-            packetPackCount,
-            packets[0:packetPackSize],
-        ),
-        PacketPack(
-            packetPackCount,
-            packets[packetPackSize : packetPackSize * 2],
-        ),
-        PacketPack(
-            packetPackCount,
-            packets[packetPackSize * 2 :],
-        ),
-    ]
-
-    # print("packetPackSize", packetPackSize)
-    # print("packetPackCount", packetPackCount)
-    # print("len(packetsStr)", len(packetsStr))
-    # print("len(packets)", len(packets))
-    #
-    # for packetIdx in range(packetPackCount * packetSize):
-    #     print("%0.8d:" % packetIdx, end="	")
-    #     print(
-    #         "".join(
-    #             ["%0.2X " % i for i in packets[packetIdx + 0 * packetPackSize]]
-    #         ),
-    #         end="	",
-    #     )
-    #     print(
-    #         "".join(
-    #             ["%0.2X " % i for i in packets[packetIdx + 1 * packetPackSize]]
-    #         ),
-    #         end="	",
-    #     )
-    #     print(
-    #         "".join(
-    #             [
-    #                 "%0.2X "
-    #                 % abs(
-    #                     packets[packetIdx + 0 * packetPackSize][i]
-    #                     - packets[packetIdx + 1 * packetPackSize][i]
-    #                 )
-    #                 for i in range(packetSize)
-    #             ]
-    #         ),
-    #         end="	",
-    #     )
-    #     print()
+class dpiValue(Enum):
+    D200 = 0x01
+    D400 = 0x02
+    D600 = 0x03
+    D800 = 0x04
+    D1000 = 0x05
+    D1200 = 0x06
+    D1600 = 0x07
+    D2000 = 0x09
+    D2400 = 0x0B
+    D3200 = 0x0D
+    D4000 = 0x0E
+    D4800 = 0x0F
 
 
 dev = usb.core.find(idVendor=deviceInfo.vendor, idProduct=deviceInfo.product)
@@ -119,16 +84,85 @@ intf = cfg[(0, 0)]
 # dev.detach_kernel_driver(intf.bInterfaceNumber)
 usb.util.claim_interface(dev, intf.bInterfaceNumber)
 
-# write the data
-packetPacks = get_packets_to_send()
-for i in packetPacks[1].packets:
+
+def sendPacket(packet):
     dev.ctrl_transfer(
         deviceInfo.bRequestType,
         deviceInfo.bRequest,
         deviceInfo.wIndex,
         intf.bInterfaceNumber,
-        i,
+        packet,
     )
+
+
+def sendColorPacket(idx, r, g, b):
+    r = 0xF - r
+    g = 0xF - g
+    b = 0xF - b
+    r, g = g, r
+    packet = [
+        0x07,
+        0x14,
+        (((2 * idx) << 4) + r) & 0xFF,
+        ((g << 4) + b) & 0xFF,
+        0,
+        0,
+        0,
+        0,
+    ]
+    sendPacket(packet)
+
+
+def sendKeybindPacket(k: key, b: keybind):
+    sendPacket([0x07, 0x10, k.value, b.value, 0, 0, 0, 0])
+
+
+defaultMouseMode = 0x05
+enabledMouseModes = 0b00111111
+
+
+def sendMouseModePacket(modeToChange, dpi: dpiValue):
+    sendPacket(
+        [
+            0x07,
+            0x10,
+            (defaultMouseMode + 0x3F) & 0xFF,
+            ((dpi.value << 4) | (modeToChange + 7)) & 0xFF,
+            0,
+            0,
+            0,
+        ]
+    )
+
+
+# keybinds
+sendKeybindPacket(key.LMB, keybind.LEFTCLICK)
+sendKeybindPacket(key.MMB, keybind.MIDDLECLICK)
+sendKeybindPacket(key.RMB, keybind.RIGHTCLICK)
+sendKeybindPacket(key.FORWARD, keybind.FORWARD)
+sendKeybindPacket(key.BACKWARD, keybind.BACKWARD)
+sendKeybindPacket(key.PLUS, keybind.DPI_PLUS)
+sendKeybindPacket(key.MINUS, keybind.DPI_MINUS)
+# mouse mode
+sendPacket([0x07, 0x09, 0x44, 0x18, 0x3F, 0, 0, 0])
+sendPacket([0x07, 0x09, 0x44, 0x39, 0x3F, 0, 0, 0])
+sendPacket([0x07, 0x09, 0x44, 0x5A, 0x3F, 0, 0, 0])
+sendPacket([0x07, 0x09, 0x44, 0x7B, 0x3F, 0, 0, 0])
+sendPacket([0x07, 0x09, 0x44, 0xBC, 0x3F, 0, 0, 0])
+sendPacket([0x07, 0x09, 0x44, 0xFD, 0x3F, 0, 0, 0])
+
+
+# colors
+
+
+sendColorPacket(0x0, 0x1, 0x1, 0x1)
+sendColorPacket(0x1, 0x4, 0x4, 0x5)
+sendColorPacket(0x2, 0xC, 0xD, 0xF)
+sendColorPacket(0x3, 0x4, 0x7, 0xF)
+sendColorPacket(0x4, 0x6, 0xE, 0x6)
+sendColorPacket(0x5, 0xF, 0x4, 0x6)
+# static led
+sendPacket([0x07, 0x13, 0x7F, 0x86, 0, 0, 0, 0])
 
 usb.util.release_interface(dev, intf.bInterfaceNumber)
 dev.attach_kernel_driver(intf.bInterfaceNumber)
