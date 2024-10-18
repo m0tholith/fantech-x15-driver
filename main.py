dab528fd1542646df6c003de0e2117c0a2ec4fcb
m0tholith <48518572+m0tholith@users.noreply.github.com>
2024-10-18 10:44:15 +0300

    Moved to using class instead of randomly placed functions

--- main.py
+++ main.py
@@ -6,14 +6,6 @@ import usb.core
 import usb.util
 
 
-class deviceInfo:
-    vendor = 0x18F8
-    product = 0x0FC0
-    bRequestType = 0x21
-    bRequest = 0x09
-    wIndex = 0x0307
-
-
 class key(Enum):
     LMB = 0x01
     SCROLLBUTTON = 0x02
@@ -62,119 +54,127 @@ ledModes = {
     "cyclic": 0x96,
 }
 
-dev = usb.core.find(idVendor=deviceInfo.vendor, idProduct=deviceInfo.product)
 
-# was it found?
-if dev is None:
-    raise ValueError("Device not found")
+class mouse:
+    def __init__(self) -> None:
+        self.vendor = 0x18F8
+        self.product = 0x0FC0
+        self.bRequestType = 0x21
+        self.bRequest = 0x09
+        self.wIndex = 0x0307
 
-# fix Resource Busy error
-for cfg in dev:
-    for intf in cfg:
-        if dev.is_kernel_driver_active(intf.bInterfaceNumber):
-            try:
-                dev.detach_kernel_driver(intf.bInterfaceNumber)
-            except usb.core.USBError as e:
-                sys.exit(
-                    "Could not detatch kernel driver from interface({0}): {1}".format(
-                        intf.bInterfaceNumber, str(e)
-                    )
-                )
+        self.defaultMouseMode = 0x02
+        self.enabledMouseModes = 0b00111111
 
-# set the active configuration. With no arguments, the first
-# configuration will be the active one
-dev.set_configuration()
+        dev = usb.core.find(idVendor=self.vendor, idProduct=self.product)
+        if dev is None:
+            raise ValueError("Device not found")
+        self.dev = dev
 
-# get an endpoint instance
-cfg = dev.get_active_configuration()
-intf = cfg[(0, 0)]
+        # fix Resource Busy error
+        for cfg in dev:
+            for intf in cfg:
+                if dev.is_kernel_driver_active(intf.bInterfaceNumber):
+                    try:
+                        dev.detach_kernel_driver(intf.bInterfaceNumber)
+                    except usb.core.USBError as e:
+                        sys.exit(
+                            "Could not detatch kernel driver from interface({0}): {1}".format(
+                                intf.bInterfaceNumber, str(e)
+                            )
+                        )
+        dev.set_configuration()
+        cfg = dev.get_active_configuration()
+        self.intf = cfg[(0, 0)]
 
-# dev.detach_kernel_driver(intf.bInterfaceNumber)
-usb.util.claim_interface(dev, intf.bInterfaceNumber)
+        usb.util.claim_interface(dev, self.intf.bInterfaceNumber)
 
+    def __enter__(self):
+        return self
 
-def sendPacket(packet):
-    dev.ctrl_transfer(
-        deviceInfo.bRequestType,
-        deviceInfo.bRequest,
-        deviceInfo.wIndex,
-        intf.bInterfaceNumber,
-        packet,
-    )
+    def __exit__(self, exc_type, exc_value, traceback):
+        usb.util.release_interface(self.dev, self.intf.bInterfaceNumber)
+        self.dev.attach_kernel_driver(self.intf.bInterfaceNumber)
 
+    def sendPacket(self, packet):
+        self.dev.ctrl_transfer(
+            self.bRequestType,
+            self.bRequest,
+            self.wIndex,
+            self.intf.bInterfaceNumber,
+            packet,
+        )
 
-def sendColorPacket(idx, rgb):
-    rgb = 0xFFF - rgb
-    r = (rgb & 0xF00) >> 8
-    g = (rgb & 0x0F0) >> 4
-    b = (rgb & 0x00F) >> 0
-    r, g = g, r
-    packet = [
-        0x07,
-        0x14,
-        (((2 * idx) << 4) + r) & 0xFF,
-        ((g << 4) + b) & 0xFF,
-        0,
-        0,
-        0,
-        0,
-    ]
-    sendPacket(packet)
-
-
-def sendKeybindPacket(k: key, b):
-    sendPacket([0x07, 0x10, k.value, b, 0, 0, 0, 0])
-
-
-defaultMouseMode = 0x02
-enabledMouseModes = 0b00111111
-
-
-def sendMouseModePacket(modeToChange, dpi):
-    sendPacket(
-        [
+    def sendColorPacket(self, idx, rgb):
+        rgb = 0xFFF - rgb
+        r = (rgb & 0xF00) >> 8
+        g = (rgb & 0x0F0) >> 4
+        b = (rgb & 0x00F) >> 0
+        r, g = g, r
+        packet = [
             0x07,
-            0x09,
-            (defaultMouseMode + 0x3F) & 0xFF,
-            ((dpi << 4) | (modeToChange + 7)) & 0xFF,
-            enabledMouseModes & 0xFF,
+            0x14,
+            (((2 * idx) << 4) + r) & 0xFF,
+            ((g << 4) + b) & 0xFF,
+            0,
             0,
             0,
             0,
         ]
-    )
+        self.sendPacket(packet)
 
+    def sendKeybindPacket(self, k: key, b):
+        self.sendPacket([0x07, 0x10, k.value, b, 0, 0, 0, 0])
 
-def sendLedModePacket(mode, time=0):
-    sendPacket([0x07, 0x13, 0x7F, (mode - time) & 0xFF, 0, 0, 0, 0])
+    def sendMouseModePacket(self, modeToChange, dpi):
+        self.sendPacket(
+            [
+                0x07,
+                0x09,
+                (defaultMouseMode + 0x3F) & 0xFF,
+                ((dpi << 4) | (modeToChange + 7)) & 0xFF,
+                self.enabledMouseModes & 0xFF,
+                0,
+                0,
+                0,
+            ]
+        )
 
+    def sendLedModePacket(self, mode, time=0):
+        self.sendPacket([0x07, 0x13, 0x7F, (mode - time) & 0xFF, 0, 0, 0, 0])
 
-def sendFireSpeedPacket(rate):
-    sendPacket([0x07, 0x12, 0x00, (rate * 37 // 300) & 0xFF, 0, 0, 0, 0])
+    def sendFireSpeedPacket(self, rate):
+        self.sendPacket(
+            [0x07, 0x12, 0x00, (rate * 37 // 300) & 0xFF, 0, 0, 0, 0]
+        )
 
 
 parsedConfig = toml.load("./config.toml")
 
-# keybinds
-sendKeybindPacket(key.LMB, keybinds[parsedConfig["keymaps"]["lmb"]])
-sendKeybindPacket(
-    key.SCROLLBUTTON, keybinds[parsedConfig["keymaps"]["scrollbutton"]]
-)
-sendKeybindPacket(key.RMB, keybinds[parsedConfig["keymaps"]["rmb"]])
-sendKeybindPacket(key.FORWARD, keybinds[parsedConfig["keymaps"]["forward"]])
-sendKeybindPacket(key.BACKWARD, keybinds[parsedConfig["keymaps"]["backward"]])
-sendKeybindPacket(key.PLUS, keybinds[parsedConfig["keymaps"]["plus"]])
-sendKeybindPacket(key.MINUS, keybinds[parsedConfig["keymaps"]["minus"]])
-# mouse mode
-defaultMouseMode = parsedConfig["misc"]["defaultmode"]
-for i in range(len(parsedConfig["modes"])):
-    sendMouseModePacket(i - 1, dpiValues[parsedConfig["modes"][i]["dpi"]])
-    sendColorPacket(i, parsedConfig["modes"][i]["color"])
-# led mode
-sendLedModePacket(
-    ledModes[parsedConfig["led"]["type"]], parsedConfig["led"]["time"]
-)
-sendFireSpeedPacket(parsedConfig["misc"]["fireSpeed"])
-
-usb.util.release_interface(dev, intf.bInterfaceNumber)
-dev.attach_kernel_driver(intf.bInterfaceNumber)
+with mouse() as m:
+    # keybinds
+    m.sendKeybindPacket(key.LMB, keybinds[parsedConfig["keymaps"]["lmb"]])
+    m.sendKeybindPacket(
+        key.SCROLLBUTTON, keybinds[parsedConfig["keymaps"]["scrollbutton"]]
+    )
+    m.sendKeybindPacket(key.RMB, keybinds[parsedConfig["keymaps"]["rmb"]])
+    m.sendKeybindPacket(
+        key.FORWARD, keybinds[parsedConfig["keymaps"]["forward"]]
+    )
+    m.sendKeybindPacket(
+        key.BACKWARD, keybinds[parsedConfig["keymaps"]["backward"]]
+    )
+    m.sendKeybindPacket(key.PLUS, keybinds[parsedConfig["keymaps"]["plus"]])
+    m.sendKeybindPacket(key.MINUS, keybinds[parsedConfig["keymaps"]["minus"]])
+    # mouse mode
+    defaultMouseMode = parsedConfig["misc"]["defaultmode"]
+    for i in range(len(parsedConfig["modes"])):
+        m.sendMouseModePacket(
+            i - 1, dpiValues[parsedConfig["modes"][i]["dpi"]]
+        )
+        m.sendColorPacket(i, parsedConfig["modes"][i]["color"])
+    # led mode
+    m.sendLedModePacket(
+        ledModes[parsedConfig["led"]["type"]], parsedConfig["led"]["time"]
+    )
+    m.sendFireSpeedPacket(parsedConfig["misc"]["fireSpeed"])
