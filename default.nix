63df952487adbae7dd785b52b9a962988187a9ae
m0tholith <48518572+m0tholith@users.noreply.github.com>
2024-10-22 16:19:50 +0300

    Removed unnecessary LED mode "fixed" (same as "static" with 0 time)

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ python3Packages }:
+{ lib, python3Packages }:
 python3Packages.buildPythonApplication {
   pname = "fantech-x15-driver";
   version = "0.1.0";
@@ -9,4 +9,11 @@ python3Packages.buildPythonApplication {
   ];
 
   src = ./.;
+
+  meta = {
+    description = "Driver for Fantech X15";
+    homepage = "https://github.com/m0tholith/fantech-x15-driver";
+    license = lib.licenses.gpl3Only;
+    mainProgram = "fantech-x15-driver";
+  };
 }
